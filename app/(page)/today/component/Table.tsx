f765136485336150408ed548284a7705bfeacfa8
shakil5281 <shakilhossen307@gmail.com>
2024-09-08 21:55:37 +0600

    update api

--- app/(page)/today/component/Table.tsx
+++ app/(page)/today/component/Table.tsx
@@ -3,7 +3,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -40,13 +40,13 @@ const fetchTimeEntries = async () => {
 const calculateHours = (inTime: string, outTime: string) => {
   const [inHour, inMinute] = inTime.split(':').map(Number);
   const [outHour, outMinute] = outTime.split(':').map(Number);
-  
+
   const inDate = new Date();
   inDate.setHours(inHour, inMinute);
-  
+
   const outDate = new Date();
   outDate.setHours(outHour, outMinute);
-  
+
   const diffMs = outDate.getTime() - inDate.getTime();
   return (diffMs / (1000 * 60 * 60)).toFixed(2); // Return hours as a string with 2 decimal places
 };
@@ -109,15 +109,25 @@ export default function ElectricityHoursTable() {
                 <TableCell>
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                      <button aria-haspopup="true" className="btn-icon p-4">
+                      <button aria-haspopup="true" className="btn-icon p-3">
                         <MoreHorizontal className="h-4 w-4" />
                         <span className="sr-only">Toggle menu</span>
                       </button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
+                    <DropdownMenuContent className="w-52 p-2" align="end">
                       <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
-                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                      <DropdownMenuItem className="p-0">
+                        <div className="flex space-x-4 justify-start items-center hover:bg-sky-400 hover:text-white w-full p-2 rounded-md duration-200 cursor-pointer">
+                          <Edit className="h-4 w-4" />
+                          <span>Edit</span>
+                        </div>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem className="p-0">
+                        <div className="flex space-x-4 justify-start items-center hover:bg-destructive hover:text-destructive-foreground w-full p-2 rounded-md duration-200 cursor-pointer">
+                          <Trash className="h-4 w-4" />
+                          <span>Delete</span>
+                        </div>
+                      </DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </TableCell>
