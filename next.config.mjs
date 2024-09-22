15a589c51126b8a842259dcc872613eb4b934e5f
shakil <shakilhossen307@gmail.com>
2024-09-22 12:42:23 +0600

    update package

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     images: {
       domains: ['static.vecteezy.com'], // Add the domain you want to allow
     },
